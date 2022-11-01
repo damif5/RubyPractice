ab586909492b13a12f7229f1c10e2f43992282db
Dami Afolabi <dami.afolabi@icloud.com>
2022-11-01 10:47:12 -0400

    reorganized files
    Signed-off-by: Dami Afolabi <dami.afolabi@icloud.com>

--- Practice.rb
+++ Practice.rb
@@ -52,3 +52,11 @@ class MegaGreeter
         mg.say_bye
     end
     
+    puts "Hello"
+puts "Enter Your Name"
+puts "Enter A Number"
+num1 = gets.chomp()
+puts "Enter another number"
+num2 = gets.chomp()
+
+puts (num1 + num2)
--- Ruby/learnruby.rb
+++
@@ -1,8 +0,0 @@
-puts "Hello"
-puts "Enter Your Name"
-puts "Enter A Number"
-num1 = gets.chomp()
-puts "Enter another number"
-num2 = gets.chomp()
-
-puts (num1 + num2)
